7e294371c20d12329418aff6d7b56441c1fc387e
amrhoballah <58459749+amrhoballah@users.noreply.github.com>
2020-12-29 16:06:31 +0200

    edits

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path')
 const fs = require('fs')
 const { findSourceMap } = require('module')
 const app = express() 
+var currentUser
 
 app.use(express.urlencoded());
 app.use(express.static('public'))
@@ -15,6 +16,7 @@ app.set('view engine', 'ejs')
 
 app.get('/', function(req, res){ 
 
+    res.render('login',{message : ""}) 
 }) 
 app.get('/registration', function(req, res){ 
 
@@ -57,7 +59,6 @@ app.get('/poetry', function(req, res){
     res.render('poetry') 
 }) 
 app.get('/readlist', function(req, res){ 
-    res.render('readlist') 
 }) 
 app.get('/searchresults', function(req, res){ 
 
@@ -82,9 +83,21 @@ app.post('/register', function(request, res){
         }
     }
     data.push(user)
-    fs.writeFileSync('users.json', JSON.stringify(data))
+    fs.writeFileSync('users.json', JSON.stringify(data,null,2))
     res.render('login',{message : "You have registered successfully"})
     
+})
+app.post('/login',function(request, res){
+    var user = {username: request.body.username, password: request.body.password}
+    var data = JSON.parse(fs.readFileSync("users.json"))
+    for( i in data){
+        if (user.username == data[i].username && user.password == data[i].password){
+            currentUser = user.username
+            res.render('home')
+            return
+        }
+    }
+    res.render('login',{message : "The username or password are incorrect"})
 })
 
 app.listen(8080, function(error){ 
