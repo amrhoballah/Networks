77520e9021dc116de9213b32ecddb6219f80042a
amrhoballah <58459749+amrhoballah@users.noreply.github.com>
2020-12-29 15:31:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -87,4 +87,4 @@ app.post('/register', function(request, res){
     
 })
 
-app.listen(port, () => console.log('app listening on port {$port}!'))
+app.listen(port, () => console.log('app listening on port ${port}!'))
