8869eefb16c9f17a881d1fb34559923d90e87781
amrhoballah <58459749+amrhoballah@users.noreply.github.com>
2020-12-31 02:53:58 +0200

    Batates

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
 const express = require('express') 
 const path = require('path') 
 const fs = require('fs')
-const session = require('express-session')
-const {MongoClient} = require('mongodb');
-const uri = 'mongodb+srv://amrhoballah:tesco2012@project.uutww.mongodb.net/project?retryWrites=true&w=majority'
-const database = new MongoClient(uri)
+const { findSourceMap } = require('module')
 const app = express() 
 var currentUser
+
 app.use(express.urlencoded());
 app.use(express.static('public'))
 app.use(express.json());
-app.use(session({
-    secret : "shhhhh"
-}))
+
   
 app.set('views', path.join(__dirname, 'views')) 
 app.set('view engine', 'ejs') 
 
-
-app.get('/', async(req, res) =>{ 
-    try {
-        await database.connect();
-    } catch (error) {
-        console.log(error);
+app.post('/addtoreadinglist',function(request, res){
+    var data = JSON.parse(fs.readFileSync("users.json"))
+    for( i in data){
+        if (currentUser == data[i].username){
+            for(j in data[i].readinglist)
+                if(data[i].readinglist[j].path == request.body.path){
+                    res.render(request.body.path)
+                    return
+                }
+            data[i].readinglist.push({path:request.body.path,name: request.body.name})
+        }
     }
+    fs.writeFileSync('users.json', JSON.stringify(data))
+    res.render(request.body.path)
+})
+app.get('/', function(req, res){ 
+
     res.render('login',{message : ""}) 
 }) 
 app.get('/registration', function(req, res){ 
@@ -66,105 +72,57 @@ app.get('/poetry', function(req, res){
 
     res.render('poetry') 
 }) 
-app.get('/readlist', async(req, res) =>{ 
-    var data
-    try{
-        data = await database.db("Users").collection("Users").findOne({username: req.session.name})
+app.get('/readlist', function(req, res){ 
+    var data = JSON.parse(fs.readFileSync("users.json"))
+    var list
+    for( i in data){
+        if (currentUser == data[i].username)
+            list = data[i].readinglist
     }
-    catch(error){
-        console.log(error)
-    }
-    res.render('readlist',{data: data.readinglist }) 
+    res.render('readlist',{data: list }) 
 }) 
 app.get('/searchresults', function(req, res){ 
 
-    res.render('searchresults',{data:[]}) 
+    res.render('searchresults') 
 }) 
 app.get('/sun', function(req, res){ 
 
     res.render('sun') 
 })
 
-app.post('/register', async(req, res) =>{
-    var user = {username: req.body.username, password: req.body.password, readinglist: []}
-    var result
-    try{
-        result = await database.db("Users").collection("Users").findOne({username: user.username})
+app.post('/register', function(request, res){
+    var user = {username: request.body.username, password: request.body.password, readinglist: []}
+    var data = JSON.parse(fs.readFileSync("users.json"))
+    for( i in data){
+        if (user.username == data[i].username){
+            res.render('registration',{message : "This username is already taken"})
+            return;
+        }
+        if (user.username == "" || user.password == ""){
+            res.render('registration',{message : "Please enter a username and a password"})
+            return;
+        }
     }
-    catch(error){
-        console.log(error)
-    }
-    if (result){
-        res.render('registration',{message : "This username is already taken"})
-        return;
-    }
-    if (user.username == "" || user.password == ""){
-        res.render('registration',{message : "Please enter a username and a password"})
-        return;
-    }
-
-    try{
-        await database.db("Users").collection("Users").insertOne(user)
-    }
-    catch(error){
-        console.log(error)
-    }
+    data.push(user)
+    fs.writeFileSync('users.json', JSON.stringify(data,null,2))
     res.render('login',{message : "You have registered successfully"})
     
 })
-app.post('/login',async (req, res) =>{
-    var user = {username: req.body.username, password: req.body.password}
-    var result
-    try{
-        result = await database.db("Users").collection("Users").findOne({username : user.username, password: user.password})
-    }
-    catch(error){
-        console.log(error)
-    }
-    if (result){
-        req.session.name = user.username
-        res.render('home')
-        return
-    }
-    res.render('login',{message : "The username or password are incorrect"})
-})
-app.post('/addtoreadinglist',async(req, res)=>{
-    var data
-    try{
-        data = await database.db("Users").collection("Users").findOne({username: req.session.name})
-    }
-    catch(error){
-        console.log(error)
-    }
-    for(i in data.readinglist){
-        if(data.readinglist[i].path == req.body.path){
-            res.render(req.body.path)
+var loggedin
+app.post('/login',function(request, res){
+    var user = {username: request.body.username, password: request.body.password}
+    var data = JSON.parse(fs.readFileSync("users.json"))
+    for( i in data){
+        if (user.username == data[i].username && user.password == data[i].password){
+            loggedin = user.username
+            res.render('home')
             return
         }
     }
-    data.readinglist.push({path:req.body.path,name: req.body.name})
-    try{
-        await database.db("Users").collection("Users").updateOne({ username: req.session.name }, {$set: {readinglist: data.readinglist} })
-    }
-    catch(error){
-        console.log(error)
-    }
-    res.render(req.body.path)
-})
-app.post('/search',function(req, res){
-    var x = (req.body.Search).toLocaleLowerCase()
-    var list =[]
-    var data = [{"path":"flies","name":"Lord of the Flies"},{"path":"grapes","name":"The Grapes of Wrath"},{"path":"leaves","name":"Leaves of Grass"},{"path":"sun","name":"The Sun and Her Flowers"},{"path":"dune","name":"Dune"},{"path":"mockingbird","name":"To Kill a Mockingbird"}]
-    for(i in data){
-        if(data[i].name.toLocaleLowerCase().includes(x)){
-            list.push(data[i])
-        }
-    }
-    res.render('searchresults',{data: list})
+    res.render('login',{message : "The username or password are incorrect"})
 })
 
-
-const port = process.env.PORT || 5001;
-
-app.listen(port, () => console.log('Server is listening on port ${port}...'));
-
+app.listen(8080, function(error){
+    if(error) throw error 
+    console.log("localhost:8080 is waiting for you") 
+}) 
