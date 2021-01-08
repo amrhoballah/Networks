fbf592c42829e2515a1b3591aa93f616b5f39aaa
amrhoballah <58459749+amrhoballah@users.noreply.github.com>
2021-01-08 03:19:30 +0200

    Fine tuning

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const path = require('path')
 const session = require('express-session')
 const {MongoClient} = require('mongodb');
 const uri = 'mongodb+srv://amrhoballah:tesco2012@project.uutww.mongodb.net/project?retryWrites=true&w=majority'
-const database = new MongoClient(uri)
+const database = new MongoClient(uri,{useUnifiedTopology : true})
 const app = express() 
 
-app.use(express.urlencoded());
+app.use(express.urlencoded({
+    extended : true
+}));
 app.use(express.static('public'))
 app.use(express.json());
 app.use(session({
@@ -25,15 +27,15 @@ app.get('/', async(req, res) =>{
     } catch (error) {
         console.log(error);
     }
-    res.render('login',{message : ""}) 
+    res.render('login') 
 }) 
 app.get('/registration', function(req, res){ 
 
-    res.render('registration', {message: ""}) 
+    res.render('registration') 
 })
 app.get('/dune', function(req, res){ 
 
-    res.render('dune', {message: ""}) 
+    res.render('dune') 
 }) 
 app.get('/fiction', function(req, res){ 
 
@@ -41,11 +43,11 @@ app.get('/fiction', function(req, res){
 })
 app.get('/flies', function(req, res){
 
-    res.render('flies', {message: ""}) 
+    res.render('flies') 
 }) 
 app.get('/grapes', function(req, res){ 
 
-    res.render('grapes',{message: ""})
+    res.render('grapes')
 }) 
 app.get('/home', function(req, res){ 
 
@@ -53,11 +55,11 @@ app.get('/home', function(req, res){
 }) 
 app.get('/leaves', function(req, res){ 
 
-    res.render('leaves', {message: ""}) 
+    res.render('leaves') 
 }) 
 app.get('/mockingbird', function(req, res){ 
 
-    res.render('mockingbird', {message: ""}) 
+    res.render('mockingbird') 
 }) 
 app.get('/novel', function(req, res){ 
 
@@ -83,7 +85,7 @@ app.get('/searchresults', function(req, res){
 }) 
 app.get('/sun', function(req, res){ 
 
-    res.render('sun', {message: ""}) 
+    res.render('sun') 
 })
 
 app.post('/register', async(req, res) =>{
@@ -96,11 +98,13 @@ app.post('/register', async(req, res) =>{
         console.log(error)
     }
     if (result){
-        res.render('registration',{message : "This username is already taken"})
+        //res.render('registration',{message : "This username is already taken"})
+        res.send('<script>alert("This username is already taken"); window.location.href="/registration";</script>');
         return;
     }
     if (user.username == "" || user.password == ""){
-        res.render('registration',{message : "Please enter a username and a password"})
+        //res.render('registration',{message : "Please enter a username and a password"})
+        res.send('<script>alert("Please enter a username and a password"); window.location.href="/registration";</script>');
         return;
     }
 
@@ -110,7 +114,8 @@ app.post('/register', async(req, res) =>{
     catch(error){
         console.log(error)
     }
-    res.render('login',{message : "You have registered successfully"})
+    //res.render('login',{message : "You have registered successfully"})
+    res.send('<script>alert("You have registered successfully"); window.location.href="/";</script>');
     
 })
 app.post('/login',async (req, res) =>{
@@ -127,19 +132,23 @@ app.post('/login',async (req, res) =>{
         res.render('home')
         return
     }
-    res.render('login',{message : "The username or password are incorrect"})
+    //res.render('login',{message : "The username or password are incorrect"})
+    res.send('<script>alert("The username or password are incorrect"); window.location.href="/";</script>');
+
+    
 })
 app.post('/addtoreadinglist',async(req, res)=>{
-    var data
+    let data
     try{
         data = await database.db("Users").collection("Users").findOne({username: req.session.name})
     }
     catch(error){
         console.log(error)
     }
-    for(let i in data.readinglist){
-        if(data.readinglist[i].path == req.body.path){
-            res.render(req.body.path,{message : "You have already added this book"})
+    for(let book of data.readinglist){
+        if(book.path == req.body.path){
+            //res.render(req.body.path,{message : "You have already added this book"})
+            res.send('<script>alert("You have already added this book"); window.location.href="/readlist";</script>');
             return
         }
     }
@@ -150,15 +159,15 @@ app.post('/addtoreadinglist',async(req, res)=>{
     catch(error){
         console.log(error)
     }
-    res.render(req.body.path,{message: ""})
+    res.send('<script>alert("You have successfully added this book"); window.location.href="/readlist";</script>');
 })
 app.post('/search',function(req, res){
-    var x = (req.body.Search).toLocaleLowerCase()
+    let keyword = (req.body.Search).toLocaleLowerCase()
     var list =[]
     var data = [{"path":"flies","name":"Lord of the Flies"},{"path":"grapes","name":"The Grapes of Wrath"},{"path":"leaves","name":"Leaves of Grass"},{"path":"sun","name":"The Sun and Her Flowers"},{"path":"dune","name":"Dune"},{"path":"mockingbird","name":"To Kill a Mockingbird"}]
-    for(let i in data){
-        if(data[i].name.toLocaleLowerCase().includes(x)){
-            list.push(data[i])
+    for(let book of data){
+        if(book.name.toLocaleLowerCase().includes(keyword)){
+            list.push(book)
         }
     }
     res.render('searchresults',{data: list})
@@ -167,5 +176,5 @@ app.post('/search',function(req, res){
 
 const port = process.env.PORT || 8080;
 
-app.listen(port, () => console.log('Server is listening on port ${port}...'));
+app.listen(port, () => console.log(`Server is listening on port ${port}...`));
 
